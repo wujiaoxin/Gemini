bf9571571ba1534b663e22d7a8c135016eb40d04
WeijunFu <anyxx@qq.com>
2016-11-30 15:45:11 +0800

    update 1.完成移动端查看订单界面；2.添加进度查询前端部分

--- application/mobile/controller/Order.php
+++ application/mobile/controller/Order.php
@@ -22,6 +22,25 @@ class Order extends Base {
 	}
 
 	public function index() {		
+		$map = '';
+		$uid = session('user_auth.uid');
+		if($uid > 0){
+			$map = 'uid = '.$uid.' or bank_uid = '.$uid;
+		}else{
+			return $this->error('请重新登录');
+		}
+		
+		$order = "id desc";
+		$list  = db('Order')->where($map)->order($order)->paginate(10);
+
+		$data = array(
+			'list' => $list,
+			'page' => $list->render(),
+		);
+		$this->assign($data);
+		return $this->fetch();
+	}
+	public function progress() {
 		//$this->assign($user);		
 		return $this->fetch();
 	}
@@ -53,6 +72,42 @@ class Order extends Base {
 			$this->assign($data);
 			$this->assign('title', '新建订单');
 			return $this->fetch('edit');
+		}
+	}
+	//查看
+	public function view() {
+		$link = model('Order');
+		$id   = input('id', '', 'trim,intval');
+		$map = '';
+		$uid = session('user_auth.uid');
+		if($uid > 0){
+			$map = '(uid = '.$uid.' or bank_uid = '.$uid.') and id = '.$id;
+		}else{
+			return $this->error('请重新登录');
+		}
+
+		if (IS_POST) {
+			$data = input('post.');
+			if ($data) {
+				$result = $link->save($data, array('id' => $data['id']));
+				if ($result) {
+					return $this->success("修改成功！", url('Order/index'));
+				} else {
+					return $this->error("修改失败！");
+				}
+			} else {
+				return $this->error($link->getError());
+			}
+		} else {
+			//$map  = array('id' => $id);
+			$info = db('Order')->where($map)->find();
+
+			$data = array(
+				'keyList' => $link->keyList,
+				'info'    => $info,
+			);
+			$this->assign($data);
+			return $this->fetch();
 		}
 	}
 }
