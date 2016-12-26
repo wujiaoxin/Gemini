b3b19b8c0bc930b5580e37adab1a3867b8549a9e
WeijunFu <anyxx@qq.com>
2016-12-26 17:43:09 +0800

    update  1.更新默认缓存目录

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ define('ICONV_ENABLED', TRUE);
  * 缓存目录设置
  * 此目录必须可写，建议移动到非WEB目录
  */
-define ( 'RUNTIME_PATH', __DIR__ . '../../../thinkphp/data/' );
+define ( 'RUNTIME_PATH', __DIR__ . '/data/' );
 
 // 加载框架引导文件
 require __DIR__ . '/core/start.php';
