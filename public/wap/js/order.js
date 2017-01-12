b7e7335ecbd7640c72e27e009addeeb1e486b048
wujiaoxin <jinxin.wang@towser.cn>
2017-01-12 16:31:35 +0800

    update   1.pc首页样式微调  2.部分函数整合至order.js

--- public/wap/js/order.js
+++ public/wap/js/order.js
@@ -13,3 +13,47 @@ function hideOrder(id,type){
         }
     });	
 }
+
+//身份证识别
+function tryOCR(id,type){
+            if(type != 'idcard_face'){
+                return;
+            }
+            ajax_jquery({
+                type: 'post',
+                url: '/mobile/open/tryOCR',
+                datatype: 'json',
+                data: {
+                    "id": id
+                },
+                success: function(data){
+                    if(data.code == 1){
+                        $('#idcard_name').val(data.data.name);
+                        $('#idcard_num').val(data.data.idcard_num);
+                    }else{
+                        if (typeof(data.msg) == 'string') {
+                            ui_alert(data.msg);
+                        }
+                    }
+                }
+            });
+        };
+
+//时间戳格式化 2010-12-22 03:34:21
+function formatDate(timeStr){
+    var timeStr = timeStr*1000;
+    var now =new Date(timeStr);
+    var year=now.getFullYear();     
+    var month=now.getMonth()+1;     
+    var date=now.getDate();     
+    var hour=fillZero(now.getHours());     
+    var minute=fillZero(now.getMinutes());     
+    var second=fillZero(now.getSeconds());    
+    return   year+"-"+month+"-"+date+"   "+hour+":"+minute+":"+second;     
+};
+function fillZero(i){
+        if (i<10){
+            i="0" + i;
+        }
+        return i
+    }
