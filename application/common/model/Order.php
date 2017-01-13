958f409ef8182497a54c3b8008c7722311b1fd12
WeijunFu <anyxx@qq.com>
2017-01-13 15:40:15 +0800

    fixed 1.修改审核订单排序

--- application/common/model/Order.php
+++ application/common/model/Order.php
@@ -60,7 +60,8 @@ class Order extends \app\common\model\Base {
 			$filter['type'] =['<',3];
 		}
 		$filter['status'] = ['>',-1];
-		$list = db('OrderAuth')->alias('a')->join('Order b','a.order_id = b.id','LEFT')->where($filter)->paginate(15);
+		$sort = "id desc";
+		$list = db('OrderAuth')->alias('a')->join('Order b','a.order_id = b.id','LEFT')->where($filter)->order($sort)->paginate(15);
 		return $list;
 	}
 	
