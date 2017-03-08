a98a8612d5c2935c7794865473baa75a64186f21
wujiaoxin <jinxin.wang@towser.cn>
2017-03-08 16:10:49 +0800

    update 1.添加新版车商员工页面 2.添加车商管理后台框架

--- application/mobile/controller/User.php
+++ application/mobile/controller/User.php
@@ -227,4 +227,25 @@ TODO:check idcard if needed;
 			return $this->fetch();
 		}
 	}
+
+	// 二维码页面
+	public function QRCode() {
+		//$this->assign($user);		
+		return $this->fetch();
+	}
+	// 个人中心-我页面
+	public function me() {
+		//$this->assign($user);		
+		return $this->fetch();
+	}
+	// 我的积分页面
+	public function myPoints() {
+		//$this->assign($user);		
+		return $this->fetch();
+	}
+	//我的客户
+	public function myCustomers() {
+		//$this->assign($user);		
+		return $this->fetch();
+	}
 }
