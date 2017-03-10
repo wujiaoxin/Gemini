5ca830067a2f14f905968a15edef36d989b0ff3d
liqiang <lq@52wzb.com>
2017-03-10 18:22:04 +0800

    update 1. 我的账户-充值提现

---
+++ application/business/controller/Account.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | SentCMS [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.tensent.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: molong <molong@tensent.cn> <http://www.tensent.cn>
+// +----------------------------------------------------------------------
+
+namespace app\business\controller;
+use app\common\controller\Base;
+
+class Account extends Base {
+	public function index() {
+		return $this->fetch();
+	}
+	
+	public function cashValue() {
+		return $this->fetch();
+	}
+
+}
