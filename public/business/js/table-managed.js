180efc5c7a432d8c8e3742207362364638ef42c3
wujiaoxin <jinxin.wang@towser.cn>
2017-03-16 23:43:05 +0800

    update 1.添加车商管理后台商户、项目等页面;

---
+++ public/business/js/table-managed.js
@@ -0,0 +1,168 @@
+var TableManaged = function () {
+
+    return {
+
+        //main function to initiate the module
+        init: function () {      
+            if (!jQuery().dataTable) {
+                return;
+            }
+            // 我的员工页面-我的员工table  id="table-myStaff"
+            $('#table-myStaff').dataTable({
+                "aoColumns": [
+                    { "bSortable": true },
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+    				{ "bSortable": false }
+                ],
+                "aLengthMenu": [
+                    [5, 10, 15, 20, -1],
+                    [5, 10, 15, 20, "全部"] // change per page values here
+                ],
+                // set the initial value
+                "iDisplayLength": 5,
+                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                "sPaginationType": "bootstrap",
+                "oLanguage": {
+                    "sLengthMenu": "_MENU_ 条记录每页",
+                    "oPaginate": {
+                        "sPrevious": "上一页",
+                        "sNext": "下一页"
+                    }
+                },
+                "aoColumnDefs": [{
+                        'bSortable': true,
+                        'aTargets': [0]
+                    }
+                ]
+            });
+
+            jQuery('#table-myStaff_wrapper .dataTables_filter input').addClass("m-wrap medium"); // modify table search input
+            jQuery('#table-myStaff_wrapper .dataTables_length select').addClass("m-wrap small"); // modify table per page dropdown
+            // jQuery('#table-myStaff_wrapper .dataTables_length select').select2(); // initialzie select2 dropdown
+
+
+            // 借款项目-借款项目table  id="table-loanItem"
+            $('#table-loanItem').dataTable({
+                "aoColumns": [
+                    { "bSortable": true },
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null   
+                ],
+                "aLengthMenu": [
+                    [5, 10, 15, 20, -1],
+                    [5, 10, 15, 20, "全部"]
+                ],
+                // set the initial value
+                "iDisplayLength": 5,
+                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                "sPaginationType": "bootstrap",
+                "oLanguage": {
+                    "sLengthMenu": "_MENU_ 条记录每页",
+                    "oPaginate": {
+                        "sPrevious": "上一页",
+                        "sNext": "下一页"
+                    }
+                },
+                "aoColumnDefs": [{
+                        'bSortable': true,
+                        'aTargets': [0]
+                    }
+                ]
+            });
+
+            jQuery('#table-loanItem_wrapper .dataTables_filter input').addClass("m-wrap medium");
+            jQuery('#table-loanItem_wrapper .dataTables_length select').addClass("m-wrap small");
+            
+
+            // 还款项目-还款项目table  id="table-repayItem"
+            $('#table-repayItem').dataTable({
+                "aoColumns": [
+                    { "bSortable": true },
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null   
+                ],
+                "aLengthMenu": [
+                    [5, 10, 15, 20, -1],
+                    [5, 10, 15, 20, "全部"]
+                ],
+                // set the initial value
+                "iDisplayLength": 5,
+                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                "sPaginationType": "bootstrap",
+                "oLanguage": {
+                    "sLengthMenu": "_MENU_ 条记录每页",
+                    "oPaginate": {
+                        "sPrevious": "上一页",
+                        "sNext": "下一页"
+                    }
+                },
+                "aoColumnDefs": [{
+                        'bSortable': true,
+                        'aTargets': [0]
+                    }
+                ]
+            });
+
+            jQuery('#table-repayItem_wrapper .dataTables_filter input').addClass("m-wrap medium");
+            jQuery('#table-repayItem_wrapper .dataTables_length select').addClass("m-wrap small");
+
+
+            // 支付项目-支付项目table  id="table-payItem"
+            $('#table-payItem').dataTable({
+                "aoColumns": [
+                    { "bSortable": true },
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null   
+                ],
+                "aLengthMenu": [
+                    [5, 10, 15, 20, -1],
+                    [5, 10, 15, 20, "全部"]
+                ],
+                // set the initial value
+                "iDisplayLength": 5,
+                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                "sPaginationType": "bootstrap",
+                "oLanguage": {
+                    "sLengthMenu": "_MENU_ 条记录每页",
+                    "oPaginate": {
+                        "sPrevious": "上一页",
+                        "sNext": "下一页"
+                    }
+                },
+                "aoColumnDefs": [{
+                        'bSortable': true,
+                        'aTargets': [0]
+                    }
+                ]
+            });
+
+            jQuery('#table-payItem_wrapper .dataTables_filter input').addClass("m-wrap medium");
+            jQuery('#table-payItem_wrapper .dataTables_length select').addClass("m-wrap small");
+
+
+
+        }
+    };
+}();
---
+++ public/business/js/validate.js
@@ -0,0 +1,50 @@
+//验证手机号
+function validatePhoneNumber(mobile) {
+    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1})|145|147|222)+\d{8})$/;
+    if (!myreg.test(mobile)) {
+        return false;
+    } else {
+        return true
+    }
+}
+
+function validateBankNum(banknum) {
+    if (banknum.length < 16 || banknum.length > 19) {
+        return false;
+    } else {
+        var num = /^\d*$/;  //全数字
+        if (!num.exec(banknum)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+
+//验证身份证格式
+function validateIdCard(pId) {
+//检查身份证号码
+
+    var arrVerifyCode = [1, 0, "X", 9, 8, 7, 6, 5, 4, 3, 2];
+    var Wi = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+    var Checker = [1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1];
+
+    if (pId.length != 15 && pId.length != 18)    return false;
+
+    var Ai = pId.length == 18 ? pId.substring(0, 17) : pId.slice(0, 6) + "19" + pId.slice(6, 16);
+
+    if (!/^\d+$/.test(Ai))  return false;
+
+    var yyyy = Ai.slice(6, 10), mm = Ai.slice(10, 12) - 1, dd = Ai.slice(12, 14);
+
+    var d = new Date(yyyy, mm, dd), now = new Date();
+    var year = d.getFullYear(), mon = d.getMonth(), day = d.getDate();
+
+    if (year != yyyy || mon != mm || day != dd || d > now || year < 1940) return false;
+
+    for (var i = 0, ret = 0; i < 17; i++)  ret += Ai.charAt(i) * Wi[i];
+    Ai += arrVerifyCode[ret %= 11];
+
+    return pId.length == 18 && pId != Ai ? false : true;
+}
