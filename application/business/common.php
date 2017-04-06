1abbe1df2b3cc2519f276452d15e256c57d85834
wyf@52wzb.com <Wyf156371>
2017-04-06 16:36:28 +0800

    update 1.更新充值和提现接口2.更新我的账户方法命名修改

--- application/business/common.php
+++ application/business/common.php
@@ -1,2 +1,13 @@
 <?php
-
+    /*
+     *查询真实姓名
+     * */
+    function serch_real($uid){
+        $real = db('member')->where('uid',$uid)->find();
+        return $real['realname'];
+    }
+  function editmd($mid,$content){
+        $result = db('member')->where('uid',$mid)->update($content);
+          eco($result);
+        return $result;
+  }
