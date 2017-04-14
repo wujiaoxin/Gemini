94c421e410f7a98f21475e73ebe225658db4f130
wujiaoxin <jinxin.wang@towser.cn>
2017-04-14 21:35:16 +0800

    update 1.新建管理后台客户管理页面

---
+++ application/admin/controller/customer.php
@@ -0,0 +1,216 @@
+<?php
+// +----------------------------------------------------------------------
+// | SentCMS [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.tensent.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: molong <molong@tensent.cn> <http://www.tensent.cn>
+// +----------------------------------------------------------------------
+
+namespace app\admin\controller;
+use app\common\controller\Admin;
+
+class Customer extends Admin {
+
+	public function index($type = 3, $status = null) {
+		
+		if($status == null){
+			$filter['status'] = ['>',-1];
+		}else{
+			$filter['status'] = $status;
+		}
+		
+		/*if($type == 3){
+			$filter['type'] = $type;
+		}else{
+			$filter['type'] =['<',3];
+		}*/
+		
+		if (!IS_ROOT) {
+			$uid = session('user_auth.uid');
+			$role = session('user_auth.role');
+			if($role==1){//报单人-车商经理
+				$filter['uid'] = $uid;
+				$list  = db('Order')->where($filter)->order("id desc")->paginate(10);
+			}else{
+				$Order = model('Order');
+				$list = $Order->get_all_order_list($uid, $role, $status);
+			}
+		}else{
+			$list  = db('Order')->where($filter)->order("id desc")->paginate(10);
+		}
+		
+		$data = array(
+			'infoStr' =>json_encode($list) ,
+		);
+		$this->assign($data);
+		$this->setMeta("客户管理");
+		return $this->fetch();
+	}
+	
+/*
+	//添加
+	public function add() {
+		$link = model('Order');
+		if (IS_POST) {
+			$data = input('post.');
+			$uid = session('user_auth.uid');
+			if($uid > 0){
+				$data['uid'] = $uid;
+			}
+			if ($data) {
+				unset($data['id']);
+				$data['sn'] = $link->build_order_sn();
+				$result = $link->save($data);
+				if ($result) {
+					return $this->success("新建成功！", url('Order/index'));
+				} else {
+					return $this->error($link->getError());
+				}
+			} else {
+				return $this->error($link->getError());
+			}
+		} else {
+			$data = array(
+				'keyList' => $link->keyList,
+			);
+			$this->assign($data);
+			$this->setMeta("新建订单");
+			return $this->fetch('public/edit');
+		}
+	}
+
+	//修改
+	public function edit() {
+		$link = model('Order');
+		$id   = input('id', '', 'trim,intval');
+		if (IS_POST) {
+			$data = input('post.');
+			if ($data) {
+				$result = $link->save($data, array('id' => $data['id']));
+				if ($result) {
+					return $this->success("修改成功！", url('Order/index'));
+				} else {
+					return $this->error("修改失败！");
+				}
+			} else {
+				return $this->error($link->getError());
+			}
+		} else {
+			$map  = array('id' => $id);
+			$info = db('Order')->where($map)->find();
+
+			$data = array(
+				'keyList' => $link->keyList,
+				'info'    => $info,
+			);
+			$this->assign($data);
+			$this->setMeta("编辑订单");
+			return $this->fetch('public/edit');
+		}
+	}
+*/
+	//查看
+	public function view() {
+		
+		//checkOrderAuth
+		
+		$uid = session('user_auth.uid');
+		$role = session('user_auth.role');
+		$id   = input('id', '', 'trim,intval');
+		$info = null;
+		
+		if($uid == null){
+			return $this->error('请重新登录');
+		}
+		
+		$filter['id'] = $id;
+		
+		if (IS_ROOT) {//管理员
+			$info = db('Order')->where($filter)->find();
+		}else{
+			if($role==1){//报单人-车商经理
+				$filter['uid'] = $uid;
+				$info = db('Order')->where($filter)->find();
+			}else{
+				$authfilter['order_id'] = $id;
+				$authfilter['auth_uid'] = $uid;
+				$authfilter['auth_role'] = $role;			
+				$auth = db('OrderAuth')->where($authfilter)->find();
+				if($auth != null){
+					$info = db('Order')->where("id",$id)->find();
+				}
+			}
+
+		}
+		if($info == null){
+			return $this->error('没有查看该订单权限');
+		}
+		
+		//end checkOrderAuth
+		$Order = model('Order');
+		if (IS_POST) {
+			
+			$data = input('post.');
+			if ($data) {
+				$result = $Order->save($data, array('id' => $data['id']));
+				if ($result) {
+					return $this->success("修改成功！", url('Order/index'));
+				} else {
+					return $this->error("修改失败！");
+				}
+			} else {
+				return $this->error($Order->getError());
+			}
+		} else {			
+			$fileFilter['order_id'] = $info['id'];
+			$fileFilter['status'] = 1;//有效文件
+			$files = db('OrderFiles')->field('id,path,size,create_time,form_key,form_label')->where($fileFilter)->limit(100)->select();
+			$data = array(
+						'keyList' => $Order->keyList,
+						'info'    => $info,
+						'files'   => $files,
+						'role'    => $role,
+			);
+			$this->assign($data);
+			$this->setMeta("客户管理");
+			return $this->fetch();
+		}
+	}
+	
+	/**
+	 * 设置审核状态
+	 */
+	public function status($id, $status) {
+		$model = model('Order');
+
+		$map['id'] = $id;
+		$result    = $model::where($map)->setField('status', $status);
+		if (false !== $result) {
+			if($status == 1){//审核通过
+				$OrderExtend = model('OrderExtend');
+				$OrderExtend->addByBank($id);
+			}			
+			return $this->success("操作成功！");
+		} else {
+			return $this->error("操作失败！！");
+		}
+	}
+
+	//删除
+	public function delete() {
+		$id = $this->getArrayParam('id');
+		if (empty($id)) {
+			return $this->error('非法操作！');
+		}
+		$link = db('Order');
+
+		$map    = array('id' => array('IN', $id));
+		$result = $link->where($map)->delete();
+		if ($result) {
+			return $this->success("删除成功！");
+		} else {
+			return $this->error("删除失败！");
+		}
+	}
+}
---
+++ application/admin/static/js/table-managed.js
@@ -0,0 +1,57 @@
+var TableManaged = function () {
+
+    return {
+
+        //main function to initiate the module
+        init: function () {      
+            if (!jQuery().dataTable) {
+                return;
+            }
+            // 我的员工页面-我的员工table  id="table-customer"
+            $('#table-customer').dataTable({
+                "aoColumns": [
+                    { "bSortable": true },
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+    				{ "bSortable": false }
+                ],
+                "aLengthMenu": [
+                    [5, 10, 15, 20, -1],
+                    [5, 10, 15, 20, "全部"] // change per page values here
+                ],
+                // set the initial value
+                "iDisplayLength": 5,
+                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                "sPaginationType": "bootstrap",
+                "oLanguage": {
+                    "sLengthMenu": "每页显示 _MENU_ 条记录",
+                    "sZeroRecords": "抱歉， 没有找到",
+                    "sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+                    "sInfoEmpty": "没有数据",
+                    "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
+                    "oPaginate": {
+                        "sFirst": "首页",
+                        "sPrevious": "前一页",
+                        "sNext": "后一页",
+                        "sLast": "尾页"
+                    },
+                    "sZeroRecords": "没有检索到数据"
+                },
+                "aoColumnDefs": [{
+                        'bSortable': true,
+                        'aTargets': [0]
+                    }
+                ]
+            });
+
+            jQuery('#table-customer_wrapper .dataTables_filter input').addClass("m-wrap medium"); // modify table search input
+            jQuery('#table-customer_wrapper .dataTables_length select').addClass("m-wrap small"); // modify table per page dropdown
+            // jQuery('#table-customer_wrapper .dataTables_length select').select2(); // initialzie select2 dropdown
+
+        }
+    };
+}();
