d81112beaf89bad08602cfd2a9108a8fed070b0c
WeijunFu <anyxx@qq.com>
2017-04-20 16:53:45 +0800

    update 1.添加还款列表接口样例

--- application/api/controller/Repay.php
+++ application/api/controller/Repay.php
@@ -1,11 +1,4 @@
 <?php
-// +----------------------------------------------------------------------
-// | SentCMS [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2013 http://www.tensent.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: molong <molong@tensent.cn> <http://www.tensent.cn>
-// +----------------------------------------------------------------------
 
 namespace app\api\controller;
 use app\common\controller\Api;
@@ -19,18 +12,37 @@ class Repay extends Api {
 	}	
 	
 	public function getList() {
-		$resp['code'] = 0;
-		$resp['msg'] = '未知错误';
+
 		
-		$data["total"] = "0";
-		$data["per_page"] = "15";
-		$data["current_page"] = "1";
-		
-		$resp['code'] = 1;
-		$resp['msg'] = '获取成功';
-		$resp['data'] = $data;
- 
-		return json($resp);
+		$resp = '{
+			"code": 1,
+			"msg": "获取成功！",
+			"data": {
+				"total": 2,
+				"per_page": 15,
+				"current_page": 1,
+				"data": [
+					{
+						"name": "新车分期",
+						"period": 1,
+						"totalperiod": 3,
+						"monthpay": 2000,
+						"time": "2017-02-04",
+						"isrepaid": 1
+					},
+					{
+						"name": "二手车垫资",
+						"period": 1,
+						"totalperiod": 1,
+						"monthpay": 10000,
+						"time": "2017-07-04",
+						"isrepaid": 0
+					}
+				]
+			}
+		}';
+		$resp = json_decode($resp);
+		return $resp;
 	}
 
 	
