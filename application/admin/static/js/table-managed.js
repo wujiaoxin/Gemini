b81016fb4e9ccb8af03244827efb6e85c577fd9e
wujiaoxin <dd@DDs-MacBook-Pro.lan>
2017-05-17 16:07:47 +0800

    fixed 1.修复格式化日期函数参数为空返回值问题; 2.增加代扣审核操作控制

--- application/admin/static/js/table-managed.js
+++ application/admin/static/js/table-managed.js
@@ -260,8 +260,6 @@ var TableManaged = function () {
                     null,
                     null,
                     null,
-                    null,
-                    null,
                     { "bSortable": false }
                 ],
                 configData
