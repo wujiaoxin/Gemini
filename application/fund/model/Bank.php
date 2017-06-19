369d839bc965f91142b1674477a57eb62ce13ee7
wujiaoxin <dd@DDs-MacBook-Pro.lan>
2017-06-19 16:26:10 +0800

    update 1.增加资金方后台

---
+++ application/fund/model/Bank.php
@@ -0,0 +1,48 @@
+<?php
+// +----------------------------------------------------------------------
+// | SentCMS [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.tensent.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: molong <molong@tensent.cn> <http://www.tensent.cn>
+// +----------------------------------------------------------------------
+
+namespace app\common\model;
+
+/**
+* 模型基类
+*/
+class Bank extends \think\Model{
+
+	protected $name = "Dealer";
+	/**
+	 * 银行卡判断
+	 * @return 私户或者公户信息
+	 */
+	public function get_bank($bank_name){
+
+		$priv_bank_account_id = $this->where('priv_bank_account_id',$bank_name)->find();
+
+		$bank_account_id = $this->where('bank_account_id',$bank_name)->find();
+
+
+		if ($priv_bank_account_id) {
+
+			$datas['dealer_bank'] = $priv_bank_account_id['priv_bank_name'];
+
+			$datas['dealer_bank_branch'] = $priv_bank_account_id['priv_bank_branch'];
+
+		}elseif ($bank_account_id) {
+
+			$datas['dealer_bank'] = $bank_account_id['bank_name'];
+
+			$datas['dealer_bank_branch'] = $bank_account_id['bank_branch'];
+		}else{
+
+			$datas['dealer_bank'] = '';
+
+			$datas['dealer_bank_branch'] = '';
+		}
+		return $datas;
+	}
+}
